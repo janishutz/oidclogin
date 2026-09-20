996f4959fbf12bc910fe774cb2f4224fdf956d13
Janis Hutz <development@janishutz.com>
2026-09-20 10:59:57 +0200

    feat: stubs also call user function on login

--- stubs.go
+++ stubs.go
@@ -1,22 +1,25 @@
 package oidclogin
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+)
 
 func startStubs(r *gin.Engine) {
-	r.GET("/auth/v2/login", func(ctx *gin.Context) {
-		redir := ctx.Query("returnTo")
-		if redir != "" {
-			ctx.Redirect(307, redir)
-		} else {
-			ctx.Redirect(307, defaultRedirect)
-		}
-	})
-	r.GET("/auth/v2/verify", func(ctx *gin.Context) {
-		redir := ctx.Query("returnTo")
-		if redir != "" {
-			ctx.Redirect(307, redir)
-		} else {
-			ctx.Redirect(307, defaultRedirect)
-		}
-	})
+	r.GET("/auth/v2/login", stubsHandler)
+	r.GET("/auth/v2/verify", stubsHandler)
+}
+
+func stubsHandler(c *gin.Context) {
+	redir := c.Query("returnTo")
+	session := sessions.Default(c)
+	userFunc("stubs", "Stubs User", "example@example.com")
+	session.Set("jhid_auth", true)
+	session.Set("jhid_uid", "stubs")
+	session.Save()
+	if redir != "" {
+		c.Redirect(307, redir)
+	} else {
+		c.Redirect(307, defaultRedirect)
+	}
 }
